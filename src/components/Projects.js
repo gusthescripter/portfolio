cd72421323856b190d8e818940c80a4cb6c1fb98
Gus <86584127+gusthescripter@users.noreply.github.com>
2022-05-18 16:02:31 -0700

    Update Projects.js
    Initial file structure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,1 +1,14 @@
-1
+import React, { Component } from 'react'
+
+export class extends Projects Component {
+    state = {
+        projects: [],
+        is loaded: false
+    }
+    componentDidMount() {
+        axios.get('')
+        .then
+        .cache;
+    }
+    
+}
