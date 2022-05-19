1a6acfcffd1e5b18345c6b35cd4fe57c324eefd3
Gus <86584127+gusthescripter@users.noreply.github.com>
2022-05-18 18:59:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React, { Component } from 'react';
+import Projects from './components/projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Portfolio</h1>
+      <Projects />
     </div>
   );
 }
